33d7772b9bc8dec5202fe900e6d7ef225816c5ed
erri120 <erri120@fossmailer.de>
2025-05-20 12:00:54 +0200

    Update script

--- scripts/create-sources.sh
+++ scripts/create-sources.sh
@@ -4,4 +4,9 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 REPO_DIR=$( dirname -- "${SCRIPT_DIR}")
 UPSTREAM_DIR="$REPO_DIR/upstream"
 
-python3 flatpak-builder-tools/dotnet/flatpak-dotnet-generator.py --dotnet 9 --runtime linux-x64 --freedesktop 24.08 "$REPO_DIR/nuget-sources.json" "$UPSTREAM_DIR/src/NexusMods.App/NexusMods.App.csproj"
+if [[ $# -eq 1 ]]; then
+	UPSTREAM_DIR="$1"
+fi
+
+echo "using upstream dir $UPSTREAM_DIR"
+python3 "$REPO_DIR/flatpak-builder-tools/dotnet/flatpak-dotnet-generator.py" --dotnet 9 --runtime linux-x64 --freedesktop 24.08 "$REPO_DIR/nuget-sources.json" "$UPSTREAM_DIR/src/NexusMods.App/NexusMods.App.csproj"
